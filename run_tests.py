1e7ffbc6d7b4108e664398b08f0e8aa7980d7b3c
SU\tatiana.kovalova <>
2019-07-09 11:28:28 +0300

    RestApiCore > run_tests fixed

--- run_tests.py
+++ run_tests.py
@@ -8,27 +8,15 @@ from settings import Settings
 if __name__ == '__main__':
     module = 'tests'
 
-    allure_folder, result_folder = Settings().ALLURE_RESULTS_PATH, Settings().API_RESULTS_PATH
-
-    for folder in [allure_folder, result_folder]:
-        try:
-            os.makedirs(folder, 0o777)
-            os.chmod(folder, 0o777)
-            print(f'- bash       | makedirs {folder}')
-        except OSError:
-            os.chmod(folder, 0o777)
-            print(f'- bash       | chmod  {folder}')
-    print('')
-
     argv = [sys.argv[0],
             '--verbosity=0',
             '--nocapture',
             '--logging-level=WARN',
             '--logging-filter=browser',
             '--with-xunit',
-            f'--xunit-file={result_folder}/nosetests.xml',
+            f'--xunit-file={Settings().API_RESULTS_PATH}/nosetests.xml',
             '--with-allure',
-            f'--logdir={allure_folder}',
+            f'--logdir={Settings().ALLURE_RESULTS_PATH}',
             '--not-clear-logdir',
             module]
 
